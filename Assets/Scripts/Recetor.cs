8ba67f59cac66f17c15f707b47fc8e165f8dcc30
JCHACKER2024 <jorge_1616_costa@hotmail.com>
2026-06-22 14:43:30 +0100

    add level 6

--- Assets/Scripts/Recetor.cs
+++ Assets/Scripts/Recetor.cs
@@ -4,6 +4,9 @@ public class ReceiverVisual : MonoBehaviour
 {
     public Texture texturaVermelha;
     public Texture texturaVerde;
+    public Light luzRecetor;
+    public Color corVermelha = Color.red;
+    public Color corVerde = Color.green;
 
     private MeshRenderer meshRenderer;
     private bool atingido = false;
@@ -16,6 +19,10 @@ public class ReceiverVisual : MonoBehaviour
     void Update()
     {
         meshRenderer.materials[0].mainTexture = atingido ? texturaVerde : texturaVermelha;
+
+        if (luzRecetor != null)
+            luzRecetor.color = atingido ? corVerde : corVermelha;
+
         atingido = false;
     }
 
